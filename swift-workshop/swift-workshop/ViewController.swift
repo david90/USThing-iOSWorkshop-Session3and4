b803f6767f04357d52fe96b4554b60a89bb05bf9
david90 <david.ng@oursky.com>
2017-01-19 01:59:46 +0800

    Set up the main login view

--- swift-workshop/swift-workshop/ViewController.swift
+++ swift-workshop/swift-workshop/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateUserStatus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateUserStatus() {
+        
+    }
+    
+    @IBAction func loginButtonDidTap(_ sender: AnyObject) {
+        print("Login!")
+    }
+    @IBAction func signupButtonDidTap(_ sender: AnyObject) {
+        print("Signup!")
     }
 
 
