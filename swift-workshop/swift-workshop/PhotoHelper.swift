fc775aef70cc8aad5c732628636c574727210c78
david90 <david.ng@oursky.com>
2017-01-20 09:38:13 +0800

    Add PhotoHelper.swift

---
+++ swift-workshop/swift-workshop/PhotoHelper.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoHelper.swift
+//  swift-workshop
+//
+//  Created by David Ng on 20/1/2017.
+//  Copyright © 2017 Skygear. All rights reserved.
+//
+
+import UIKit
+import SKYKit
+
+class PhotoHelper {
+    
+    static func resize(image: UIImage, maxWidth: CGFloat, quality: CGFloat = 1.0) -> Data? {
+        var actualWidth = image.size.width
+        var actualHeight = image.size.height
+        let heightRatio = actualHeight / actualWidth
+        
+        print("FROM: \(actualWidth)x\(actualHeight) ratio \(heightRatio)")
+        
+        if actualWidth > maxWidth {
+            actualWidth = maxWidth
+            actualHeight = maxWidth * heightRatio
+        }
+        
+        print("TO: \(actualWidth)x\(actualHeight)")
+        
+        let rect = CGRect(x: 0, y: 0, width: actualWidth, height: actualHeight)
+        UIGraphicsBeginImageContext(rect.size)
+        image.draw(in: rect)
+        guard let img = UIGraphicsGetImageFromCurrentImageContext(),
+            let imageData = UIImageJPEGRepresentation(img, quality) else {
+                return nil
+        }
+        
+        return imageData
+    }
+    
+}
